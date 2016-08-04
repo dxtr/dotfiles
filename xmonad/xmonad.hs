183570cc9b132534596c48bc5469c8030130a08b
Kim Lidström <kim.lidstrom@adlibris.com>
2016-08-04 02:24:16 +0200

    Loads of updates
    * Added some layouts
    * Renamed all layouts so they look good in xmobar
    * Added some more keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad (when)
 import Data.List (isInfixOf)
 import qualified Data.Map as M
 import System.Exit
 
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.Actions.Search
 import XMonad.Actions.WindowGo
 import XMonad.Actions.WindowBringer
@@ -17,10 +19,18 @@ import XMonad.Hooks.EwmhDesktops (ewmh, fullscreenEventHook)
 import XMonad.Layout.DwmStyle
 import XMonad.Layout.IM
 import XMonad.Layout.NoBorders
-import XMonad.Layout.Spiral
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
+import XMonad.Layout.Magnifier
+import XMonad.Layout.Dishes
+import XMonad.Layout.Accordion
+import XMonad.Layout.Reflect
+import XMonad.Layout.Grid
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.SimpleFloat
+import XMonad.Layout.Renamed
+import XMonad.Layout.NoBorders
 import qualified XMonad.StackSet as W
 import XMonad.Util.Paste
 import XMonad.Util.Run
@@ -32,6 +42,8 @@ import XMonad.Prompt.Man
 import XMonad.Prompt.Shell
 import XMonad.Prompt.Ssh
 import XMonad.Prompt.Window
+
+myMod = mod4Mask
 
 myFocusedBorderColor :: [Char]
 myFocusedBorderColor = "#ff0000"
@@ -83,7 +95,7 @@ myManageHook = composeAll [ className =? "Firefox"      --> doShift "web",
 -- myXPConfig = Prompt.def
 
 myXPConfig :: XPConfig
-myXPConfig = defaultXPConfig {
+myXPConfig = XMonad.Prompt.def {
   font = "6x12",
   bgColor = "#282a36",
   fgColor = "#f8f8f2",
@@ -96,38 +108,42 @@ myXPConfig = defaultXPConfig {
   }
 
 myKeys :: XConfig t -> M.Map (KeyMask, KeySym) (X ())
-myKeys (XConfig {modMask = m, terminal = term}) = M.fromList [
-  ((m                , xK_Insert), pasteSelection),
-  ((m                , xK_Print ), unsafeSpawn "maim -s --hidecursor $(date \"+%FT%T\").png"),
-  ((m .|. shiftMask  , xK_Print ), unsafeSpawn "maim --hidecursor $(date \"+%FT%T\").png"),
-  ((m                , xK_comma ), sendMessage (IncMasterN 1)),
-  ((m                , xK_period), sendMessage (IncMasterN (-1))),
-  ((m                , xK_n     ), refresh),
-  ((m                , xK_m     ), windows W.focusMaster  ),
-  ((m                , xK_p     ), shellPrompt myXPConfig),
-  ((m .|. shiftMask  , xK_d     ), kill),
-  ((m                , xK_j     ), windows W.focusDown),
-  ((m                , xK_k     ), windows W.focusUp),
-  ((m .|. shiftMask  , xK_j     ), windows W.swapDown),
-  ((m .|. shiftMask  , xK_k     ), windows W.swapUp),
-  ((m .|. controlMask, xK_k     ), sendMessage MirrorExpand),
-  ((m .|. controlMask, xK_j     ), sendMessage MirrorShrink),
-  ((m .|. controlMask, xK_h     ), sendMessage Shrink),
-  ((m .|. controlMask, xK_l     ), sendMessage Expand),
-  ((m                , xK_z     ), withFocused $ windows . W.sink), -- unfloat
-  ((m                , xK_w     ), raiseBrowser),
-  ((m                , xK_e     ), raiseEditor),
-  ((m                , xK_F1    ), manPrompt myXPConfig),
-  ((m                , xK_F2    ), sshPrompt myXPConfig),
-  ((m                , xK_g     ), gotoMenuArgs myDmenuGotoArgs),
-  ((m                , xK_b     ), bringMenuArgs myDmenuBringArgs),
+myKeys (XConfig {XMonad.modMask = modMask, XMonad.terminal = term}) = M.fromList [
+  ((modMask .|. shiftMask  , xK_space ), sendMessage ToggleLayout),
+  ((modMask                , xK_Right ), nextWS),
+  ((modMask                , xK_Left  ), prevWS),
+  ((modMask .|. shiftMask  , xK_Right ), shiftToNext),
+  ((modMask .|. shiftMask  , xK_Left  ), shiftToPrev),
+  ((modMask                , xK_Insert), pasteSelection),
+  ((modMask .|. shiftMask  , xK_Tab   ), toggleWS),
+  ((modMask                , xK_Print ), unsafeSpawn "maim -s --hidecursor $(date \"+%FT%T\").png"),
+  ((modMask .|. shiftMask  , xK_Print ), unsafeSpawn "maim --hidecursor $(date \"+%FT%T\").png"),
+  ((modMask                , xK_comma ), sendMessage (IncMasterN 1)),
+  ((modMask                , xK_period), sendMessage (IncMasterN (-1))),
+  ((modMask                , xK_n     ), refresh),
+  ((modMask                , xK_m     ), windows W.focusMaster  ),
+  ((modMask                , xK_p     ), unsafeSpawn "rofi -show run"),
+  ((modMask .|. shiftMask  , xK_d     ), kill),
+  ((modMask                , xK_j     ), windows W.focusDown),
+  ((modMask                , xK_k     ), windows W.focusUp),
+  ((modMask .|. shiftMask  , xK_j     ), windows W.swapDown),
+  ((modMask .|. shiftMask  , xK_k     ), windows W.swapUp),
+  ((modMask .|. controlMask, xK_k     ), sendMessage MirrorExpand),
+  ((modMask .|. controlMask, xK_j     ), sendMessage MirrorShrink),
+  ((modMask .|. controlMask, xK_h     ), sendMessage Shrink),
+  ((modMask .|. controlMask, xK_l     ), sendMessage Expand),
+  ((modMask                , xK_z     ), withFocused $ windows . W.sink), -- unfloat
+--  ((modMask                , xK_F1    ), manPrompt myXPConfig),
+--  ((modMask                , xK_F2    ), sshPrompt myXPConfig),
+--  ((modMask                , xK_g     ), gotoMenuArgs myDmenuGotoArgs),
+--  ((modMask                , xK_b     ), bringMenuArgs myDmenuBringArgs),
 --  ((m              , xK_b     ), windowPromptBring myXPConfig),
   -- XF86AudioMute
-  ((0, 0x1008ff12), spawn "/usr/bin/ponymix toggle"),
+  ((0, 0x1008ff12), spawn "/usr/bin/amixer set Master toggle"),
   -- XF86AudioLowerVolume
-  ((0, 0x1008ff11), spawn "/usr/bin/ponymix decrease 5"),
+  ((0, 0x1008ff11), spawn "/usr/bin/amixer set Master 5%-"),
   -- XF86AudioRaiseVolume
-  ((0, 0x1008ff13), spawn "/usr/bin/ponymix increase 5"),
+  ((0, 0x1008ff13), spawn "/usr/bin/amixer set Master 5%+"),
   -- XF86MonBrightnessDown
   ((0, 0x1008ff03), spawn "/usr/bin/xbacklight -dec 5"),
   -- XF86MonBrightnessUp
@@ -136,6 +152,14 @@ myKeys (XConfig {modMask = m, terminal = term}) = M.fromList [
     myDmenuArgs = ["-b", "-i", "-fn", "Terminus-9"] :: [String]
     myDmenuGotoArgs = myDmenuArgs ++ ["-p", "goto>"] :: [String]
     myDmenuBringArgs = myDmenuArgs ++ ["-p", "bring>"] :: [String]
+
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList
+  [ ((modMask, button1), (\w -> focus w >> ifFloating w mouseMoveWindow))
+  , ((modMask, button2), (\w -> focus w >> ifFloating w mouseResizeWindow))
+  , ((modMask, button3), (\w -> focus w >> windows W.swapMaster))
+  ]
+  where
+    ifFloating w f = withWindowSet $ \ws -> when (M.member w $ W.floating ws) (f w)
 
   -- , ((m,              xK_i), raiseMaybe (runInTerm "-title Irssi" "sh -c 'screen -r irssi'") (title ~? "Irssi"))
   -- , ((m .|. shiftMask,xK_i), spawn "xclip -o|tr '\n' ' '|sed -e 's/- //' -e 's/^ *//' -e 's/ *$//' -e 's/  / /' > /tmp/z.txt && screen -S 'irssi' -X readbuf /tmp/z.txt && screen -S 'irssi' -X paste .;rm /tmp/z.txt")
@@ -155,32 +179,71 @@ myStartupHook = do
   startupHook desktopConfig
   setWMName "LG3D"
 
- 
+--myLayout = avoidStruts $ smartBorders $ smartSpacing windowSpacing $ perWS
+myLayout = avoidStruts $ toggleLayouts Full $ perWS
+  where
+    windowSpacing = 2
+    layout l = smartSpacing windowSpacing $ l
+    renamedLayout name l = renamed [Replace name] $ layout l
+    tallLayout = renamedLayout "Tall" $ ResizableTall nmaster delta ratio slaves
+      where nmaster = 1
+            delta = 3/100
+            ratio = 1/2
+            slaves = []
+
+    magnifiedTallLayout = renamedLayout "Magnified Tall" $ magnifiercz' magnifyRatio $ tallLayout
+      where magnifyRatio = 1.5
+        
+    wideLayout = renamedLayout "Wide" $ Mirror $ tallLayout
+        
+    magnifiedWideLayout = renamedLayout "Magnified Wide" $ magnifiercz' magnifyRatio $ wideLayout
+      where magnifyRatio = 1.5
+        
+    dishLayout = renamedLayout "Dishes" $ Dishes nmaster ratio
+      where nmaster = 1
+            ratio = 1/5
+
+    fullLayout = renamedLayout "Full" $ noBorders Full
+    gridLayout = renamedLayout "Grid" $ Grid
+    magnifiedGridLayout = renamedLayout "Magnified Grid" $ gridLayout
+    accordionLayout = renamedLayout "Accordion" $ Accordion
+    
+    mainWS = onWorkspace "main" (magnifiedTallLayout ||| magnifiedWideLayout ||| tallLayout ||| wideLayout ||| accordionLayout)
+    codeWS = onWorkspace "code" (magnifiedWideLayout ||| magnifiedTallLayout ||| wideLayout ||| tallLayout)
+    webWS = onWorkspace "web" (fullLayout ||| tallLayout ||| wideLayout)
+    imWS = onWorkspace "im" (fullLayout ||| gridLayout ||| accordionLayout)
+    mediaWS = onWorkspace "media" (fullLayout ||| wideLayout ||| magnifiedWideLayout ||| tallLayout ||| magnifiedTallLayout)
+    gamesWS = onWorkspace "games" (simpleFloat ||| tallLayout ||| wideLayout)
+    restWS = magnifiedTallLayout ||| magnifiedWideLayout ||| tallLayout ||| wideLayout ||| accordionLayout ||| gridLayout
+    perWS = mainWS $ codeWS $ webWS $ imWS $ mediaWS $ gamesWS $ restWS
+
 settings = desktopConfig {
   terminal           = "urxvtc",
   focusFollowsMouse  = False,
   borderWidth        = 1,
   modMask            = mod4Mask,
-  workspaces         = ["main", "code", "web", "im", "media", "games" ] ++ map show [7..9],
+  workspaces         = ["main", "code", "web", "im", "media", "games"],
   normalBorderColor  = "#373844",
   focusedBorderColor = myFocusedBorderColor,
   keys               = \c -> myKeys c `M.union` XMonad.keys desktopConfig c,
   --   mouseBindings      = mouseBindings settings,
-  layoutHook         = avoidStruts $ smartBorders $ Full ||| tallLayout,
+  --layoutHook         = avoidStruts $ smartBorders $ smartSpacing 2 $ Full ||| tallLayout,
+  layoutHook         = myLayout,
   manageHook         = manageDocks <+> myManageHook <+> manageHook desktopConfig,
   startupHook        = myStartupHook,
   handleEventHook    = fullscreenEventHook
   }
-  where spir = smartSpacing 2 $ spiralWithDir direction rotation sratio
-        direction = East -- East, South, West, North
-        rotation = CCW -- CW, CCW
-        sratio = 1920/1080
-        nmaster = 1
-        delta = 3/100
-        ratio = 1/2
+
+    --spir = smartSpacing 2 $ spiralWithDir direction rotation sratio
+        --direction = East -- East, South, West, North
+        --rotation = CCW -- CW, CCW
+        --sratio = 1920/1080
+        --nmaster = 1
+        --delta = 3/100
+        --ratio = 1/2
         --tallLayout = smartSpacing 2 $ Tall nmaster delta ratio
-        tallLayout = smartSpacing 2 $ ResizableTall nmaster delta ratio []
-        spiralLayout = spiral 0
+        --tallLayout = smartSpacing 2 $ ResizableTall nmaster delta ratio []
+        --spiralLayout = spiral 0
 
 main :: IO ()
 main = do
@@ -189,7 +252,7 @@ main = do
     logHook = dynamicLogWithPP xmobarPP {
         ppOutput = hPutStrLn xmproc,
         ppCurrent = xmobarColor "#f8f8f2" "#44475a" . shorten 8,
-        ppTitle = xmobarColor "#50fa7b" "" . shorten 30,
+        ppTitle = xmobarColor "#50fa7b" "" . shorten 40,
         ppUrgent = xmobarColor "#ff5555" "" . shorten 8,
         ppSep = " | "
         }
